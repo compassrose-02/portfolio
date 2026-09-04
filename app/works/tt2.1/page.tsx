5e206fe80681995ddb75bfd22cedf2e8cecd2d60
Riley Pankow <pankow.riley.02@gmail.com>
2026-09-03 20:38:25 -0400

    mviq page finish

--- app/works/tt2.1/page.tsx
+++ app/works/tt2.1/page.tsx
@@ -15,6 +15,9 @@ import hero from './img/hero.png';
 import mviq_ui from './img/gallery/mviq_ui.jpg';
 import mviq_uithumb from './img/gallery/mviq_uithumb.jpg';
 import inspectionVisual from './img/inspection-visual.png';
+import screen1 from './img/screen1.png'
+import screen2 from './img/screen2.png'
+import screen3 from './img/screen3.png'
 
 export default function TT21() {
 
@@ -28,6 +31,7 @@ export default function TT21() {
         {
             original: mviq_ui.src,
             thumbnail: mviq_uithumb.src,
+            description: "Prototype UI on the device"
         },
     ]
 
@@ -54,8 +58,7 @@ export default function TT21() {
                         </p>
                     </section>
                     <section className="bg-gray-100" id="context">
-                        {/* TODO: make turbine image offset from left instead of right when on smaller screens
-                        TODO: parallax scroll on turbine image (maybe) */}
+                        {/* TODO: parallax scroll on turbine image (maybe) */}
                         <h3>
                             Context
                         </h3>
@@ -73,7 +76,7 @@ export default function TT21() {
                         </p>
                         <ExportedImage src={inspectionVisual} alt="Stylized render of the borescope and turning tool on a turbine" id="contextImage" />
                     </section>
-                    <section className="bg-white flex gap-10">
+                    <section className="bg-white flex gap-10" id="designGoals">
                         <div className="flex flex-col gap-5">
                             <h3>
                                 Design Goals
@@ -90,81 +93,55 @@ export default function TT21() {
                                 </p>
                             </div>
                         </div>
-                        <ExportedImage src={mviq_fd_2way} alt="Graphic of 2-way communication between turning tool and borescope" className="h-auto w-1/2 shrink-0" />
+                        <ExportedImage src={mviq_fd_2way} alt="Graphic of 2-way communication between turning tool and borescope" className="h-auto shrink-0" />
                     </section>
-                    <section className="bg-gray-100">
-
-                    </section>
-                    <section className="bg-white">
-
-                    </section>
-                    <section className="bg-gray-100">
-
-                    </section>
-                    {/* <div className="bg-white px-6 pb-10 pt-2">
+                    <section className="bg-gray-100 flex flex-col gap-5">
                         <h3>
-                            Waygate Technologies | 01/2024 - 07/2024
+                            Process
                         </h3>
-                        {
-                            // TODO: Redesign this page to go with the Figma redesign}
-                        }
-                        <div id="ColGridContainer" className="mt-10">
-                            <section id="body0" className="flex gap-2 flex-col">
-                                <h4>
-                                    Key Points
-                                </h4>
-                                <ul className="list-disc list-inside">
-                                    <li>
-                                        Designed interface for new software functionality on flagship video borescope product using Figma
-                                    </li>
-                                    <li>
-                                        Coordinated with internal and external stakeholders to integrate functions across platforms and between devices
-                                    </li>
-                                </ul>
-                            </section>
-                            <section id="body1" className="flex gap-2 flex-col">
-                                <h4>
-                                    Overview
-                                </h4>
+                        <p>
+                            I worked at the problem with the company&apos;s international design team, product stakeholders, and technicians who actually use the devices:
+                        </p>
+                        <div className="" id="screensContainer">
+                            <div>
+                                <ExportedImage src={screen1} alt="First iteration of the turning tool integration menu" className="" />
                                 <p>
-                                    During my internship at Waygate Technologies, I designed a touch interface for a new software capability of the company’s flagship video borescope: the Mentor Visual IQ+.
+                                    At first, all of the buttons made the menu cover the entire screen. Technicians couldn&apos;t see the camera feed!
                                 </p>
+                            </div>
+                            <div className="self-center">
+                                <ExportedImage src={screen2} alt="Second iteration of the turning tool integration menu" className="" />
                                 <p>
-                                    When inspecting gas turbines, technicians use a video borescope inserted into the engine casing to image the blades. The engine shaft is slowly turned using a small electric motor - a “turning tool” - to image each and every blade. The MVIQ+ has the ability to connect to a third-party turning tool and talk to it directly, enabling remote use and automatic inspection.
+                                    Splitting them into pages was better, but still bulky. It also sacrificed instant access to the second page controls.
                                 </p>
-                            </section>
-                            <section id="body2" className="flex gap-2 flex-col">
-                                <h4>
-                                    Design Process
-                                </h4>
+                            </div>
+                            <div className="self-end">
+                                <ExportedImage src={screen3} alt="Third iteration of the turning tool integration menu" className="" />
                                 <p>
-                                    During the design process I coordinated with different stakeholders to ensure that the interface met design requirements while remaining usable. The original functions of the turning tool remote needed to be included, plus the software-controlled automatic functions the MVIQ+ introduced, all on a relatively small section of the touch screen. The primary problem was conserving screen real-estate while offering a wealth of touch controls large enough for gloved hands. The balancing act of these various criteria was a fun challenge!
+                                    The breakthrough: Technicians either want all controls or a compact state readout. Let them minimize the controls, and move some to a settings page.
                                 </p>
-                                <p>
-                                    I created wireframes and interactive prototypes in Figma to explore different layouts and control schemes. After several rounds of iteration and feedback from stakeholders, I finalized the design and created detailed specifications for the development team to implement. I gained experience with developer handoff as the finished design was implemented into the software. An updated version of the UI is still present on the device, I’m proud to have something I designed be used in the wild and making an impact!
-                                </p>
-                            </section>
-                            <figure id="img1" className="flex flex-col items-center gap-1 self-center">
-                                <ExportedImage src={cfm56} alt="CFM56 Gas Turbine" className="w-full h-auto shadow-md" />
-                                <figcaption className="text-sm text-gray-500">
-                                    The CFM56 is a popular commercial gas turbine. Turning tool integration was tested on an on-site example of this model.
-                                </figcaption>
-                            </figure>
-                            <figure id="img2" className="flex flex-col items-center gap-1 self-center">
-                                <ExportedImage src={mviq_fd_2way} alt="MVIQ+ and FutureDrive NG+" className="w-full h-auto shadow-md" />
-                                <figcaption className="text-sm text-gray-500">
-                                    The MVIQ+ borescope and FutureDrive NG+ turning tool.
-                                </figcaption>
-                            </figure>
+                            </div>
                         </div>
-                    </div>
-
-                    <div className="p-6 flex flex-col gap-4">
+                    </section>
+                    <section className="bg-white flex flex-col gap-5">
+                        <h3>
+                            Results
+                        </h3>
+                        <div className="flex flex-col gap-4">
+                            <p>
+                                I also gained experience with developer handoff as the finished design was implemented into the software early 2025.
+                            </p>
+                            <p>
+                                An updated version of the UI is still present on the device out in the field, resulting in less inspector workload and enabling hands-free turbine stage inspections.
+                            </p>
+                        </div>
+                    </section>
+                    <section className="bg-gray-100 flex flex-col gap-5">
                         <div className="flex gap-1 items-center">
-                            <h4>
+                            <h3>
                                 Design Artifacts
-                            </h4>
-                            <IoMdApps size={20} />
+                            </h3>
+                            <IoMdApps size={25} />
                         </div>
                         <div className="bg-gray-200">
                             <ImageGallery
@@ -173,8 +150,7 @@ export default function TT21() {
                                 onSlide={(index) => console.log("Slid to", index)}
                             />
                         </div>
-                    </div> */}
-
+                    </section>
                 </main>
                 <footer className='px-6 py-10 bg-gray-800 flex justify-center text-white align-center'>
                     <p>&copy; {getCurrentYear()} Riley Pankow. All rights reserved.</p>
